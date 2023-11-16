b3a0cd4cd59a744de5b8379b8461c2dae6d52528
Trungcongialoc6666 <150362966+Trungcongialoc6666@users.noreply.github.com>
2023-11-16 23:05:00 +0700

    trungcode1

---
+++ admin/header.php
@@ -0,0 +1,76 @@
+<nav class="sidebar vertical-scroll  ps-container ps-theme-default ps-active-y">
+    <div class="logo d-flex justify-content-between">
+        <a href="./index.php" class="text-decoration-none">
+            <span style="color: black;" class="h4 text-uppercase  px-2">No1 - Store</span>
+        </a>
+    </div>
+    <ul id="sidebar_menu">
+        <li class="mm-active">
+            <a href="./index.php" aria-expanded="false">
+                <div></div>
+                <span>Bảng điều khiển</span>
+            </a>
+        </li>
+        <li class>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <div></div>
+                <span>Sản phẩm</span>
+            </a>
+            <ul>
+                <li><a href="?act=listproduct">Danh sách sản phẩm</a></li>
+                <li><a href="?act=addproduct">Thêm sản phẩm</a></li>
+                <li><a href="product/recycleProduct.php">Thùng rác</a></li>
+            </ul>
+        </li>
+        <li class>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <div></div>
+                <span>Danh mục</span>
+            </a>
+            <ul>
+                <li><a href="?act=listdm">Danh sách danh mục</a></li>
+                <li><a href="?act=addddm">Thêm danh mục</a></li>
+                <li><a href="category/rycycleCategory.php">Thùng rác</a></li>
+            </ul>
+        </li>
+        <li class>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <div></div>
+                <span>Tài khoản</span>
+            </a>
+            <ul>
+                <li><a href="taikhoan/account.php">Danh sách tài khoản</a></li>
+            </ul>
+        </li>
+        <li class>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <div></div>
+                <span>Bình luận</span>
+            </a>
+            <ul>
+                <li><a href="binhluan/comment.php">Danh sách bình luận</a></li>
+                <li><a href="binhluan/rycycleCmt.php">Thùng rác</a></li>
+            </ul>
+        </li>
+        <li class>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <div></div>
+                <span>Đơn hàng</span>
+            </a>
+            <ul>
+                <li><a href="orders/order.php">Danh sách đơn hàng</a></li>
+                <li><a href="#">Thùng rác</a></li>
+            </ul>
+        </li>
+        <li class>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <div></div>
+                <span>Thống kê</span>
+            </a>
+            <ul>
+                <li><a href="data_table.php">Data Tables</a></li>
+                <li><a href="bootstrap_table.php">Bootstrap</a></li>
+            </ul>
+        </li>
+    </ul>
+</nav>

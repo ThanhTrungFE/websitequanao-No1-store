7d9fdf383452c92e43c5813ab0fbe7ad53d84861
Trungcongialoc6666 <150362966+Trungcongialoc6666@users.noreply.github.com>
2023-11-16 22:37:46 +0700

    model

--- model/user.php
+++ model/user.php
@@ -1,6 +1,12 @@
-<?php 
-    function insert_taikhoan($user,$email,$password){
-        $sql = "INSERT INTO `users`( `users_name`, `email`, `password`,) VALUES ('$user','$email','$password')";
-        pdo_execute($sql);
-    }
-?>
+<?php
+function insert_taikhoan($user, $email, $password)
+{
+    $sql = "INSERT INTO `users`( `users_name`, `email`, `password`) VALUES ('$user','$email','$password')";
+    pdo_execute($sql);
+}
+function checkuser($email, $password)
+{
+    $sql = "SELECT * FROM users where email = '$email' AND password = '$password'";
+    $p = pdo_query_one($sql);
+    return $p;
+}

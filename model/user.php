c9ed9b481306399e7135a85dd08589fd5d6c8853
Trungcongialoc6666 <150362966+Trungcongialoc6666@users.noreply.github.com>
2023-11-16 22:44:27 +0700

    pdo

--- model/user.php
+++ model/user.php
@@ -3,4 +3,12 @@
         $sql = "INSERT INTO `users`( `users_name`, `email`, `password`,) VALUES ('$user','$email','$password')";
         pdo_execute($sql);
     }
+
+    function checkuser($email, $password)
+{
+    $sql = "SELECT * FROM users where email = '$email' AND password = '$password'";
+    $p = pdo_query_one($sql);
+    return $p;
+}
+
 ?>
